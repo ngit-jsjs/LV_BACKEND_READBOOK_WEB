e8392c8ce161712c5fae7bc4f7a1de23dc279d2f
ngit-jsjs <ngtien1924@gmail.com>
2026-07-03 18:13:14 +0700

    Restructure: move project to system_java_spring

--- src/main/java/org/example/lv_backend/entity/RoleName.java
+++
@@ -1,6 +0,0 @@
-package org.example.lv_backend.entity;
-
-public enum RoleName {
-    USER,
-    ADMIN
-}
--- src/main/java/org/example/lv_backend/mapper/InvalidatedTokenMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.lv_backend.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface InvalidatedTokenMapper {
-}
